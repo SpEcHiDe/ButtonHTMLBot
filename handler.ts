234929c9a6715627640a41693ee73fa91c50fd45
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2023-03-16 22:43:16 +0530

    basic thingy

--- handler.ts
+++ handler.ts
@@ -6,38 +6,12 @@ export const composer = new Composer<MyContext>();
 
 const ADD_NEW_CBDATA = "add_new";
 const TEXT_URL_SEPERATOR = "|";
+const INVALID_FIRST_MESSAGE = "invalid message";
+const SAME_URL_SEPERATOR = "|same";
 
 composer.command("start", async (ctx: MyContext) => {
     await ctx.reply("/start");
 });
-
-composer
-    .filter(
-        (ctx) => ctx.message?.chat.type === "private"
-    )
-    .on(
-        [
-            ":photo",
-            ":video",
-            ":animation",
-            ":document",
-            ":voice",
-            ":video_note",
-            ":audio",
-        ],
-        async (ctx: MyContext) => {
-            await ctx.copyMessage(
-                ctx.message?.chat.id,
-                {
-                    from_chat_id: ctx.message?.chat.id,
-                    message_id: ctx.message?.message_id,
-                    caption: ctx.message?.caption,
-                    caption_entities: ctx.message?.caption_entities,
-                    reply_to_message_id: ctx.message?.message_id,
-                }
-            );
-        }
-    );
 
 
 composer.on("callback_query", async (ctx: MyContext) => {
@@ -57,18 +31,25 @@ composer
 .on(":text", async (ctx: MyContext) => {
     let msgtextparts = ctx.message?.text.split(TEXT_URL_SEPERATOR);
     if (msgtextparts === undefined) {
-        return await ctx.reply("INVALID_ONE");
+        return await ctx.reply(INVALID_FIRST_MESSAGE);
     }
     let currentNewText: string | undefined = "";
     let currentNewUrl: string | undefined = "";
     let isSameLine: boolean = false;
     if (msgtextparts?.length > 1) {
         currentNewUrl = msgtextparts.shift();
-        currentNewUrl = currentNewUrl.trim();
+        currentNewUrl = currentNewUrl?.trim();
         currentNewText = msgtextparts?.join(TEXT_URL_SEPERATOR);
-        if (currentNewText.indexOf("|same") > -1) {
+        if (currentNewText.toLowerCase().indexOf(SAME_URL_SEPERATOR) > -1) {
             isSameLine = true;
-            currentNewText = currentNewText.replace("|same", "");
+            currentNewText = currentNewText.replace(
+                SAME_URL_SEPERATOR.toUpperCase(),
+                ""
+            );
+            currentNewText = currentNewText.replace(
+                SAME_URL_SEPERATOR.toLowerCase(),
+                ""
+            );
         }
     }
     let prevReplyMarkup = ctx.message?.reply_to_message?.reply_markup;
@@ -99,9 +80,15 @@ composer
             }]);
         }
     }
-    await ctx.editMessageReplyMarkup({
-        chat_id: ctx.message?.chat.id,
-        message_id: ctx.message?.reply_to_message.message_id,
-        reply_markup: prevReplyMarkup,
-    });
+    await ctx.copyMessage(
+        ctx.message?.chat.id,
+        {
+            from_chat_id: ctx.message?.chat.id,
+            message_id: ctx.message?.reply_to_message.message_id,
+            caption: ctx.message?.reply_to_message.caption,
+            caption_entities: ctx.message?.reply_to_message.caption_entities,
+            reply_to_message_id: ctx.message?.message_id,
+            reply_markup: prevReplyMarkup,
+        }
+    );
 });
